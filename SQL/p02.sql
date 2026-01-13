6259074994f9e2c4890c3643ef767c6b1a0e0a14
verba-neo <verba.it.corp@gmail.com>
2026-01-13 11:14:17 +0900

    Add p02 ~ 04

---
+++ SQL/p02.sql
@@ -0,0 +1,11 @@
+-- p02.sql
+
+SELECT * FROM userinfo;
+
+-- userinfo 에 email 컬럼 추가 40글자 제한, 기본값은 ex@gmail.com
+
+-- nickname 길이제한 100자로 늘리기
+
+-- reg_date 컬럼 삭제
+
+-- 실제 한명의 email 을 수정하기
---
+++ SQL/p04.sql
@@ -0,0 +1,14 @@
+-- p05.sql
+
+SELECT * FROM userinfo;
+
+ALTER TABLE userinfo ADD COLUMN age INT DEFAULT 20;
+UPDATE userinfo SET age=30 WHERE id BETWEEN 1 and 5;
+
+-- 이름 오름차순 상위 3명
+
+-- email gmail 인 사람들 나이순으로 정렬
+
+-- 나이 많은사람들 중에 핸드폰 번호 오름차순 3명의 이름, 폰번, 나이만 확인.
+
+-- 이름 오름차순 인데 가장 이름이 빠른사람 1명은 제외하고 3명만 조회. -> 페이지네이션
