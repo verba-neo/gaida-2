7c5975ff6aa96320b670ed4b6eae60e43dd0cfb3
verba-neo <verba.it.corp@gmail.com>
2026-01-20 14:24:43 +0900

    p05 lotto

--- 02-SQL/17-join.sql
+++ 02-SQL/17-join.sql
@@ -2,10 +2,44 @@
 SELECT * FROM sales;
 SELECT * FROM customers;
 
+-- 지루하고 현학적임
 SELECT
-    *,
     (
-        SELECT customer_name FROM customers as
-        WHERE customers.customer_id=sales.customer_id
-    )
-FROM sales;
+        SELECT customer_name FROM customers c
+        WHERE c.customer_id=s.customer_id
+    ) AS 고객명,
+    (
+        SELECT customer_type FROM customers c 
+        WHERE c.customer_id=s.customer_id
+    ) AS 고객등급,
+    *
+FROM sales s;
+
+-- JOIN -> 좌우 결합
+-- INNER JOIN -> 왼쪽(c)와 오른쪽(s)를 모두 만족하는 교집합 데이터.
+SELECT
+    COUNT(*)
+FROM customers c
+INNER JOIN sales s ON c.customer_id=s.customer_id;
+
+-- LEFT JOIN -> 왼쪽(c)의 모든 데이터 + (있을 경우) 매칭되는 오른쪽(s) 데이터.
+SELECT 
+    *
+FROM customers c 
+LEFT JOIN sales s ON c.customer_id=s.customer_id;
+-- WHERE s.id IS NULL;  -> 구매한 적 없는 사람만 확인
+
+-- UNION -> 상하결합
+SELECT
+    '1. INNER JOIN' AS 구분,
+    COUNT(*) AS 줄수,
+    COUNT(DISTINCT c.customer_id) AS 고객수
+FROM customers c
+INNER JOIN sales s ON c.customer_id=s.customer_id
+UNION
+SELECT
+    '2. LEFT JOIN' AS 구분,
+    COUNT(*) AS 줄수,
+    COUNT(DISTINCT c.customer_id) AS 고객수
+FROM customers c
+LEFT JOIN sales s ON c.customer_id=s.customer_id
